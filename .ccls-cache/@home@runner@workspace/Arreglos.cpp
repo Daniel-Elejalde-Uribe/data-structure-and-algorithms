7ed538da528e7fea72d825901b545c74eb9a3425
Daniel Elejalde Uribe <danielelejaldeu@gmail.com>
2025-07-20 02:27:13 +0000

    Ensayo editar arreglo

--- .ccls-cache/@home@runner@workspace/Arreglos.cpp
+++ .ccls-cache/@home@runner@workspace/Arreglos.cpp
@@ -4,5 +4,7 @@ int main(){
   int numeros[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   for (int i = 0; i < 10; i++){
     std::cout << numeros[i] << std::endl;
+    if (numeros[i] == 5)
+      break;
   }
 }
