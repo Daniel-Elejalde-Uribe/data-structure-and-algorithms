ce1482608423452718fc07173d1ef2822eed99ee
Daniel Elejalde Uribe <danielelejaldeu@gmail.com>
2025-09-04 13:59:13 +0000

    Quick y Mrrge

--- .ccls-cache/@home@runner@workspace/Ordenamiento@Quick.cpp
+++ .ccls-cache/@home@runner@workspace/Ordenamiento@Quick.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 
-// Función para intercambiar dos elementos
+// FunciÃ³n para intercambiar dos elementos
 void swap(int* a, int* b) {
     int t = *a;
     *a = *b;
     *b = t;
 }
 
-// Función de partición
+// FunciÃ³n de particiÃ³n
 int partition(int arr[], int low, int high) {
-    int pivot = arr[high]; // Elige el último elemento como pivote
-    int i = (low - 1);     // Indice del elemento más pequeÃ±o
+    int pivot = arr[high]; // Elige el Ãºltimo elemento como pivote
+    int i = (low - 1);     // Ãndice del elemento mÃ¡s pequeÃ±o
 
     for (int j = low; j <= high - 1; j++) {
-        // Si el elemento actual es más pequeño que el pivote
+        // Si el elemento actual es mÃ¡s pequeÃ±o que el pivote
         if (arr[j] < pivot) {
             i++;
             swap(&arr[i], &arr[j]);
@@ -23,19 +23,19 @@ int partition(int arr[], int low, int high) {
     return (i + 1);
 }
 
-// La función principal de Quick Sort (recursiva)
+// La funciÃ³n principal de Quick Sort (recursiva)
 void quickSort(int arr[], int low, int high) {
     if (low < high) {
-        // pi es el indice de partición
+        // pi es el Ã­ndice de particiÃ³n
         int pi = partition(arr, low, high);
 
-        // Ordena por separado los elementos antes y después de la partición
+        // Ordena por separado los elementos antes y despuÃ©s de la particiÃ³n
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
 }
 
-// Función para imprimir el arreglo
+// FunciÃ³n para imprimir el arreglo
 void printArray(int arr[], int size) {
     for (int i = 0; i < size; i++)
         std::cout << arr[i] << " ";
